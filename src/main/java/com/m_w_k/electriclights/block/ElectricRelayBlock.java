4fcb91925f81bba363a96854c279cca8d57fa704
M-W-K <mwk001@icloud.com>
2023-03-03 17:42:34 -0700

    Updated lights & relays to have placement rules, like a torch or lantern.

--- src/main/java/com/m_w_k/electriclights/block/ElectricRelayBlock.java
+++ src/main/java/com/m_w_k/electriclights/block/ElectricRelayBlock.java
@@ -9,8 +9,9 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -40,6 +41,8 @@ public class ElectricRelayBlock extends Block implements SimpleWaterloggedBlock 
     @Override
     public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos)
     {
+        // TODO implement a blown-bulb mechanism based on state changes?
+        // use 10% chance and an age blockstate, have 100% chance if waterlogged - remove the cannot glow while waterlogged logic
         if (isLight) {
             int light = state.getValue(LIGHTSTATE);
             if (light == 1 || light == 0) return light;
@@ -61,30 +64,50 @@ public class ElectricRelayBlock extends Block implements SimpleWaterloggedBlock 
         this(properties, false);
     }
     @Override
+    @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext context)
     {
-        // TODO add detection for whether the face we're trying to attach to can support us
-        BlockState state = super.getStateForPlacement(context);
-        if (state != null) {
-            Direction direction = context.getClickedFace();
-            BlockPos blockpos = context.getClickedPos();
-            FluidState fluidstate = context.getLevel().getFluidState(blockpos);
-            state.setValue(FACING, direction).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+        BlockState state = this.defaultBlockState();
+        BlockPos blockpos = context.getClickedPos();
+
+        FluidState fluidstate = context.getLevel().getFluidState(blockpos);
+        state = state.setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+
+        for (Direction candidate : context.getNearestLookingDirections()) {
+            ElectricLightsMod.logToConsole("Checking direction " + candidate.getName());
+            state = state.setValue(FACING, candidate);
+            if (state.canSurvive(context.getLevel(), blockpos)) {
+                return state;
+            }
         }
-        return state;
+        return null;
     }
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockStateBuilder) {
         blockStateBuilder.add(WATERLOGGED, FACING, LIGHTSTATE);
     }
-
+    /**
+     * Warning for "deprecation" is suppressed because the method is fine to override.
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState updateShape(BlockState state1, Direction direction, BlockState state2, LevelAccessor levelAccessor, BlockPos pos1, BlockPos pos2) {
+        return direction == state1.getValue(FACING) && !state1.canSurvive(levelAccessor, pos1) ? Blocks.AIR.defaultBlockState() : state1;
+    }
+    /**
+     * Warning for "deprecation" is suppressed because the method is fine to override.
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader levelReader, BlockPos pos) {
+        return canSupportCenter(levelReader, pos.relative(state.getValue(FACING)), state.getValue(FACING).getOpposite());
+    }
 
     @Override
     public void setPlacedBy(Level level, @NotNull BlockPos pos, @NotNull BlockState state, @Nullable LivingEntity placer, @NotNull ItemStack stack) {
         if (!level.isClientSide()) {
             ElectricLightsMod.addGraphNodeAndAutoConnect(new GraphNode(pos, isLight, false), level);
         }
-        super.setPlacedBy(level, pos, state, placer, stack);
     }
     /**
      * Warning for "deprecation" is suppressed because the method is fine to override.
