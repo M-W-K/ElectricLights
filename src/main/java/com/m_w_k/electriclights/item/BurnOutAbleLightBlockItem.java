d73a6b26ec2158d9cea146c36949083998971fa1
M-W-K <mwk001@icloud.com>
2023-03-07 10:57:34 -0700

    Fixes

--- src/main/java/com/m_w_k/electriclights/item/BurnOutAbleLightBlockItem.java
+++ src/main/java/com/m_w_k/electriclights/item/BurnOutAbleLightBlockItem.java
@@ -1,9 +1,9 @@
 package com.m_w_k.electriclights.item;
 
-import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraft.world.level.block.Block;
 
-public class BurnOutAbleLightBlockItem extends BlockItem {
+public class BurnOutAbleLightBlockItem extends ItemNameBlockItem {
     private final int initialState;
     public BurnOutAbleLightBlockItem(Block block, Properties properties, int initialState) {
         super(block, properties);
