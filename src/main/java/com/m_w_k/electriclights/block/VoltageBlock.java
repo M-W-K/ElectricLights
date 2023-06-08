91f653c108f7c9ddf140391f338956621e8f1d3f
M-W-K <mwk001@icloud.com>
2023-06-08 13:22:23 -0600

    Gave the Voltage Coil model an AABB

--- src/main/java/com/m_w_k/electriclights/block/VoltageBlock.java
+++ src/main/java/com/m_w_k/electriclights/block/VoltageBlock.java
@@ -4,14 +4,24 @@ import com.m_w_k.electriclights.blockentity.MasterSwitchboardBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class VoltageBlock extends Block {
+    protected static final VoxelShape COIL_AABB = Shapes.or(
+            Block.box(0,14,0,16,16,16),
+            Block.box(0,0,0,16,2,16),
+            Block.box(1,13,1,15,14,15),
+            Block.box(1,2,1,15,3,15),
+            Block.box(2,3,2,14,13,14));
     private final int voltage; // voltage determines light brightness and energy consumption, must be between -1 and 4 else things will break
     public VoltageBlock(Properties properties, int voltage) {
         super(properties);
@@ -45,5 +55,14 @@ public class VoltageBlock extends Block {
             }
             super.onRemove(state, level, pos, newState, isMoving);
         }
+    }
+
+    /**
+     * Warning for "deprecation" is suppressed because the method is fine to override.
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter p_153475_, BlockPos p_153476_, CollisionContext p_153477_) {
+        return COIL_AABB;
     }
 }
