7209f5f7592a13e5d0772c791edc8402723225f3
M-W-K <mwk001@icloud.com>
2023-06-14 19:10:06 -0600

    Added AABBs to the switchboard

--- src/main/java/com/m_w_k/electriclights/block/MasterSwitchboardBlock.java
+++ src/main/java/com/m_w_k/electriclights/block/MasterSwitchboardBlock.java
@@ -1,5 +1,7 @@
 package com.m_w_k.electriclights.block;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.m_w_k.electriclights.network.ELPacketHandler;
 import com.m_w_k.electriclights.network.SwitchboardHumPacket;
 import com.m_w_k.electriclights.util.ELBreaker;
@@ -18,6 +20,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -32,8 +35,13 @@ import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 public class MasterSwitchboardBlock extends BaseEntityBlock implements SimpleWaterloggedBlock, ELBreaker {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
@@ -41,6 +49,20 @@ public class MasterSwitchboardBlock extends BaseEntityBlock implements SimpleWat
     public static final Property<Integer> LIGHTSTATE = ELBlockStateProperties.LIGHTSTATE;
     public static final BooleanProperty DISABLED = ELBlockStateProperties.DISABLED;
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+
+    protected static final Map<Direction, VoxelShape> SWITCHBOARD_AABBS = Maps.newEnumMap(ImmutableMap.of(
+            Direction.SOUTH, Shapes.or(
+                    Block.box(0, 0, 0, 16, 3, 16),
+                    Block.box(0, 3, 4, 16, 16, 16)),
+            Direction.WEST, Shapes.or(
+                    Block.box(0, 0, 0, 16, 3, 16),
+                    Block.box(0, 3, 0, 12, 16, 16)),
+            Direction.NORTH, Shapes.or(
+                    Block.box(0, 0, 0, 16, 3, 16),
+                    Block.box(0, 3, 0, 16, 16, 12)),
+            Direction.EAST, Shapes.or(
+                    Block.box(0, 0, 0, 16, 3, 16),
+                    Block.box(4, 3, 0, 16, 16, 16))));
 
     public MasterSwitchboardBlock(Properties properties) {
         super(properties);
@@ -151,5 +173,14 @@ public class MasterSwitchboardBlock extends BaseEntityBlock implements SimpleWat
     public RenderShape getRenderShape(BlockState p_49232_) {
         // TODO use a BER instead of a basic model
         return RenderShape.MODEL;
+    }
+
+    /**
+     * Warning for "deprecation" is suppressed because the method is fine to override.
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter p_153475_, BlockPos p_153476_, CollisionContext p_153477_) {
+        return SWITCHBOARD_AABBS.get(state.getValue(FACING));
     }
 }
